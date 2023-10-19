8a8348f8830b82acd32f54b6cf8d5d5dd9864248
codewithmecoder <yaty1031@gmail.com>
2023-10-19 23:42:01 +0700

    create employee view

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,27 @@
+﻿using Employee.Management.ViewModels.Employees;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Employee.Management.Controllers;
+
+public class EmployeeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    } 
+    
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Create(CreateViewModel model)
+    {
+        if (ModelState.IsValid)
+        {
+            return View(model);
+        }
+        return View();
+    }
+}
---
+++ ViewModels/Employees/CreateViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Employee.Management.ViewModels.Employees;
+
+public class CreateViewModel
+{
+    [Required(ErrorMessage = "Name cannot be empty!.")]
+    public string Name { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Email cannot be empty!.")]
+    public string Email { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Address cannot be empty!.")]
+    public string Address { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Phone Number cannot be empty!.")]
+    public string PhoneNumber { get; set; } = string.Empty;
+    public IFormFile File { get; set; } = default!;
+}
